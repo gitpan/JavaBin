2e0c5d6990a0d756b7301a059e354fe052a4d1fb
James Raspass <jraspass@gmail.com>
2013-07-07 20:06:34 +0000

    Import of RAZ/JavaBin-0.1 from CPAN.
    gitpan-cpan-distribution: JavaBin
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         RAZ/JavaBin-0.1.tar.gz
    gitpan-cpan-author:       RAZ
    gitpan-cpan-maturity:     released

---
+++ lib/JavaBin.pm
@@ -0,0 +1,227 @@
+package JavaBin;
+{
+  $JavaBin::VERSION = '0.1';
+}
+# ABSTRACT: Apache Solr JavaBin (de)serializer
+
+use strict;
+use warnings;
+
+use Filter::cpp;
+
+# define BYTES($num) substr $bytes, 0, $num, ''
+# define DISPATCH &{ $dispatch_shift[ ( $tag = ord BYTES(1) ) >> 5 ] || $dispatch[$tag] }
+
+my ( $bytes, @dispatch, @dispatch_shift, @exts, $size, $tag );
+
+@dispatch = (
+    # null
+    sub { undef },
+    # bool true
+    sub { 1 },
+    # bool false
+    sub { 0 },
+    # byte
+    sub { unpack 'c', BYTES(1) },
+    # short
+    sub { unpack 's>', BYTES(2) },
+    # double
+    sub { unpack 'd>', BYTES(8) },
+    # int
+    sub { unpack 'l>', BYTES(4) },
+    # long
+    sub { unpack 'q>', BYTES(8) },
+    # float,
+    sub { unpack 'f>', BYTES(4) },
+    # date
+    sub {
+        my ( $s, $m, $h, $d, $M, $y ) = gmtime( unpack( 'q>', BYTES(8) ) / 1000 );
+
+        sprintf '%d-%02d-%02dT%02d:%02d:%02dZ', $y + 1900, $M + 1, $d, $h, $m, $s;
+    },
+    # map
+    sub { +{ map DISPATCH, 1 .. _vint() * 2 } },
+    # solr doc
+    sub { DISPATCH },
+    # solr doc list
+    sub {
+        my %result;
+
+        @result{qw/numFound start maxScore docs/} = ( @{ DISPATCH }, DISPATCH );
+
+        \%result;
+    },
+    # byte array
+    sub { [ unpack 'c*', BYTES(_vint()) ] },
+    # iterator
+    sub {
+        my @array;
+
+        push @array, &{ $dispatch_shift[ $tag >> 5 ] || $dispatch[$tag] }
+            until ( $tag = ord BYTES(1) ) == 15;
+
+        \@array;
+    },
+);
+
+# These datatypes are matched by taking the tag byte, shifting it by 5 so to only read
+# the first 3 bits of the tag byte, giving it a range or 0-7 inclusive.
+#
+# The remaining 5 bits can then be used to store the size of the datatype, e.g. how
+# many chars in a string, this therefore has a range of 0-31, if the size exceeds or
+# matches this then an additional vint is added.
+#
+# The overview of the tag byte is therefore TTTSSSSS with T and S being type and size.
+@dispatch_shift = (
+    undef,
+    # string
+    sub {
+        utf8::decode my $string = BYTES( ( $size = $tag & 31 ) == 31 ? 31 + _vint() : $size );
+
+        $string;
+    },
+    # small int
+    sub { read_small_int() },
+    # small long
+    sub { read_small_int() },
+    # array
+    sub { [ map DISPATCH, 1 .. ( ( $size = $tag & 31 ) == 31 ? 31 + _vint() : $size ) ] },
+    # ordered map
+    sub { +{ map DISPATCH, 1 .. ( ( $size = $tag & 31 ) == 31 ? 31 + _vint() : $size ) * 2 } },
+    # named list
+    sub { +{ map DISPATCH, 1 .. ( ( $size = $tag & 31 ) == 31 ? 31 + _vint() : $size ) * 2 } },
+    # extern string
+    sub {
+        if ( ( $size = $tag & 31 ) == 31 ? $size += _vint() : $size ) {
+            $exts[$size - 1];
+        }
+        else {
+            utf8::decode my $string = BYTES( ( $size = ord( BYTES(1) ) & 31 ) == 31 ? 31 + _vint() : $size );
+
+            push @exts, $string;
+
+            $string;
+        }
+    },
+);
+
+sub from_javabin($) {
+    # Read the input into $bytes whilst skipping the version byte.
+    $bytes = substr shift, 1;
+
+    @exts = ();
+
+    DISPATCH;
+}
+
+sub import {
+    no strict 'refs';
+
+    *{ caller() . '::from_javabin' } = \&from_javabin;
+}
+
+sub read_small_int {
+    my $result = $tag & 0x0f;
+
+    $result = _vint() << 4 | $result if $tag & 0x10;
+
+    $result;
+}
+
+# A private setter of bytes to allow unit testing.
+sub _bytes {
+    $bytes = pop;
+
+    shift;
+}
+
+# Lucene variable-length +ve integer, the MSB indicates whether you need another octet.
+# http://lucene.apache.org/core/old_versioned_docs/versions/3_5_0/fileformats.html#VInt
+sub _vint {
+    my ( $byte, $shift, $value );
+
+    $value = ( $byte = ord BYTES(1) ) & 127;
+
+    $value |= ( ( $byte = ord BYTES(1) ) & 127 ) << ( $shift += 7 ) while $byte & 128;
+
+    $value;
+}
+
+1;
+
+
+
+=pod
+
+=head1 NAME
+
+JavaBin - Apache Solr JavaBin (de)serializer
+
+=head1 VERSION
+
+version 0.1
+
+=head1 SYNOPSIS
+
+ use JavaBin;
+
+ my $result = from_javabin $binary_data;
+
+=head1 DESCRIPTION
+
+JavaBin is a compact binary format used by L<Apache Solr|http://lucene.apache.org/solr>.
+
+For more information on this format see the L<Solr Wiki|http://wiki.apache.org/solr/javabin>.
+
+This package provides a deserializer for this format, with a serializer planned.
+
+=head1 FUNCTIONS
+
+=head2 from_javabin
+
+ my $result = from_javabin $binary_data;
+
+Accepts one argument, a binary string of containing the JavaBin.
+
+Returns a scalar representation of the data, be that undef, number, string, or reference.
+
+This function does no error checking, hand it invalid JavaBin and it will probably die.
+
+=head1 CAVEATS
+
+Due to the differences between Java and Perl not all data structures can be mapped one-to-one.
+
+An example of such mapping is a Java interator whcih becomes a Perl array during deserialization.
+Additionally a Java HashMap, Named List, or Ordered Map will become a Perl hash.
+
+=head1 TODO
+
+=over 4
+
+=item *
+
+C<to_javabin> serializer.
+
+=item *
+
+XS implementation.
+
+=back
+
+=head1 INSPIRATION
+
+This package was inspired by the L<Ruby JavaBin library|https://github.com/kennyj/java_bin>.
+Both that library and the Java JavaBin library proved very helpful in understanding JavaBin.
+
+=head1 AUTHOR
+
+James Raspass <jraspass@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by James Raspass <jraspass@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ t/run.t
@@ -0,0 +1,98 @@
+use strict;
+use warnings;
+
+use charnames ':full';
+
+use Test::More;
+
+BEGIN { use_ok 'JavaBin' }
+
+note 'constants';
+
+is from_javabin "\0\0", undef, 'undef';
+is from_javabin "\0\1", 1, 'true';
+is from_javabin "\0\2", 0, 'false';
+
+note 'bytes';
+
+is from_javabin( "\0\3" . pack 'c', $_ ), $_, "byte $_" for qw/-128
+                                                                0
+                                                                127/;
+
+note 'shorts';
+
+is from_javabin( "\0\4" . pack 's>', $_ ), $_, "short $_" for qw/-32768
+                                                                 -129
+                                                                  0
+                                                                  128
+                                                                  32767/;
+
+note 'ints';
+
+is from_javabin( "\0\6" . pack 'l>', $_ ), $_, "int $_" for qw/-2147483648
+                                                               -8388609
+                                                               -32769
+                                                               -129
+                                                                0
+                                                                128
+                                                                32768
+                                                                8388608
+                                                                2147483647/;
+
+note 'longs';
+
+is from_javabin( "\0\7" . pack 'q>', $_ ), $_, "long $_" for qw/-9223372036854775808
+                                                                -36028797018963969
+                                                                -140737488355329
+                                                                -549755813889
+                                                                -2147483649
+                                                                -8388609
+                                                                -32769
+                                                                -129
+                                                                 0
+                                                                 128
+                                                                 32768
+                                                                 8388608
+                                                                 2147483648
+                                                                 549755813888
+                                                                 140737488355328
+                                                                 36028797018963968
+                                                                 9223372036854775807/;
+
+note 'vints';
+
+my %vints = (
+    127    => [ 0x7F ],
+    128    => [ 0x80, 0x01 ],
+    16_383 => [ 0xFF, 0x7F ],
+    16_384 => [ 0x80, 0x80, 0x01 ],
+);
+
+is( JavaBin->_bytes( pack 'C*', @{ $vints{$_} } )->_vint, $_, "vint $_" ) for sort keys %vints;
+
+note 'all';
+
+open my $fh, '<', 't/data';
+
+is_deeply from_javabin( do { local $/; <$fh> } ), {
+    array        => [qw/foo bar baz qux/],
+    byte         => 127,
+    byte_array   => [qw/-128 0 127/],
+    byte_neg     => -128,
+    date         => '1989-06-07T00:00:00Z',
+    double       => 1.797_693_134_862_31e308,
+    iterator     => [qw/qux baz bar foo/],
+    false        => 0,
+    float        => 3.402_823_466_385_29e38,
+    shifted_sint => 2_147_483_647,
+    long         =>  9_223_372_036_854_775_807,
+    long_neg     => -9_223_372_036_854_775_808,
+    null         => undef,
+    pangram      => 'The quick brown fox jumped over the lazy dog',
+    short        =>  32_767,
+    short_neg    => -32_768,
+    snowman      => "\N{SNOWMAN}",
+    true         => 1,
+}, 'from_javabin';
+
+done_testing;
