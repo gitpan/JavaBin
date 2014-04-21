750728d85a243363240a30dc4f1466b31a7af271
James Raspass <jraspass@gmail.com>
2014-04-21 17:28:34 +0000

    Import of RAZ/JavaBin-0.8 from CPAN.
    gitpan-cpan-distribution: JavaBin
    gitpan-cpan-version:      0.8
    gitpan-cpan-path:         RAZ/JavaBin-0.8.tar.gz
    gitpan-cpan-author:       RAZ
    gitpan-cpan-maturity:     released

--- example/bench.pl
+++ example/bench.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 
@@ -7,93 +9,112 @@ use Data::Dumper 'Dumper';
 use Data::MessagePack;
 use JavaBin;
 use JSON::XS qw/decode_json encode_json/;
-use Sereal;
+use Sereal qw/sereal_decode_with_object sereal_encode_with_object/;
 use Storable qw/freeze thaw/;
 use YAML::XS qw/Dump Load/;
 
 $Data::Dumper::Indent = 0;
 
+# Version is a float in order to actually bench a float.
 my $languages = {
     java => {
-        author      => 'James Gosling',
+        designers   => [ 'James Gosling' ],
         extensions  => [ qw/.jar .java .class/ ],
         native_bool => \1,
         released    => '1996-01-23',
         TIOBE_rank  => 2,
+        version     => 1.8,
+    },
+    lua => {
+        designers   => [
+            'Roberto Ierusalimschy',
+            'Waldemar Celes',
+            'Luiz Henrique de Figueiredo',
+        ],
+        extensions  => [ '.lua' ],
+        native_bool => \1,
+        released    => '1993-07-28',
+        TIOBE_rank  => 35,
+        version     => 5.2,
     },
     perl => {
-        author      => 'Larry Wall',
+        designers   => [ 'Larry Wall' ],
         extensions  => [ qw/.pl .pm .pod .t/ ],
         native_bool => \0,
         released    => '1987-12-18',
         TIOBE_rank  => 12,
+        version     => 5.18,
     },
     php => {
-        author      => 'Rasmus Lerdorf',
-        extensions  => [ qw/.php/ ],
+        designers   => [ 'Rasmus Lerdorf' ],
+        extensions  => [ '.php' ],
         native_bool => \1,
         released    => '1995-06-08',
         TIOBE_rank  => 5,
+        version     => 5.5,
     },
     python => {
-        author      => 'Guido van Rossum',
+        designers   => [ 'Guido van Rossum' ],
         extensions  => [ qw/.py .pyc .pyd .pyo .pyw/ ],
         native_bool => \1,
         released    => '1991-02-20',
         TIOBE_rank  => 8,
+        version     => 3.4,
     },
     ruby => {
-        author      => 'Yukihiro Matsumoto',
+        designers   => 'Yukihiro Matsumoto',
         extensions  => [ qw/.rb .rbw/ ],
         native_bool => \1,
         released    => '1995-12-21',
         TIOBE_rank  => 13,
+        version     => 2.1,
     },
 };
 
-my $mpack  = Data::MessagePack->new;
-my $sereal = Sereal::Encoder->new;
+my $mpack    = Data::MessagePack->new;
+my $srel_dec = Sereal::Decoder->new;
+my $srel_enc = Sereal::Encoder->new;
 
 my %alts; %alts = (
     CBOR => {
-        from => sub { decode_cbor $alts{CBOR}{data} },
-        pkg  => 'CBOR::XS',
-        to   => sub { encode_cbor $languages },
+        dec => sub { decode_cbor $alts{CBOR}{data} },
+        enc => sub { encode_cbor $languages },
+        pkg => 'CBOR::XS',
     },
     Dump => {
-        from => sub { eval $alts{Dump}{data} },
-        pkg  => 'Data::Dumper',
-        to   => sub { Dumper $languages },
+        dec => sub { eval $alts{Dump}{data} },
+        enc => sub { Dumper $languages },
+        pkg => 'Data::Dumper',
     },
     Java => {
-        from => sub { from_javabin $alts{Java}{data} },
-        pkg  => 'JavaBin',
-        to   => sub {   to_javabin $languages },
+        dec => sub { from_javabin $alts{Java}{data} },
+        enc => sub {   to_javabin $languages },
+        pkg => 'JavaBin',
     },
     JSON => {
-        from => sub { decode_json $alts{JSON}{data} },
-        pkg  => 'JSON::XS',
-        to   => sub { encode_json $languages },
+        dec => sub { decode_json $alts{JSON}{data} },
+        enc => sub { encode_json $languages },
+        pkg => 'JSON::XS',
     },
     MsgP => {
-        from => sub { $mpack->unpack($alts{MsgP}{data}) },
-        pkg  => 'Data::MessagePack',
-        to   => sub { $mpack->pack($languages) },
+        dec => sub { $mpack->unpack($alts{MsgP}{data}) },
+        enc => sub { $mpack->pack($languages) },
+        pkg => 'Data::MessagePack',
     },
     Srel => {
-        from => sub { $sereal->decode($alts{Srel}{data}) },
-        pkg  => 'Sereal',
-        to   => sub { $sereal->encode($languages) },
+        dec => sub { sereal_decode_with_object $srel_dec, $alts{Srel}{data} },
+        enc => sub { sereal_encode_with_object $srel_enc, $languages },
+        pkg => 'Sereal',
     },
     Stor => {
-        from => sub {   thaw $alts{Stor}{data} },
-        pkg  => 'Storable',
-        to   => sub { freeze $languages },
+        dec => sub {   thaw $alts{Stor}{data} },
+        enc => sub { freeze $languages },
+        pkg => 'Storable',
     },
     YAML => {
-        from => sub { Load $alts{YAML}{data} },
-        pkg  => 'YAML::XS',
-        to   => sub { Dump $languages },
+        dec => sub { Load $alts{YAML}{data} },
+        enc => sub { Dump $languages },
+        pkg => 'YAML::XS',
     },
 );
 
@@ -108,17 +129,15 @@ print "Modules\n\n";
 
 print "\nEncode\n";
 
-cmpthese -1, { map { $_ => $alts{$_}{to} } keys %alts };
+cmpthese -1, { map ref() ? $_->{enc} : $_, %alts };
 
 print "\nSize\n\n";
 
-$_->{size} = length( $_->{data} = $_->{to}->() ) for values %alts;
+$_->{size} = length( $_->{data} = $_->{enc}->() ) for values %alts;
 
-printf "%-5s%d bytes\n", $_, $alts{$_}{size}
+printf "%-5s%4d bytes\n", $_, $alts{$_}{size}
     for sort { $alts{$b}{size} <=> $alts{$a}{size} } keys %alts;
 
 print "\nDecode\n";
 
-$sereal = Sereal::Decoder->new;
-
-cmpthese -1, { map { $_ => $alts{$_}{from} } keys %alts };
+cmpthese -1, { map ref() ? $_->{dec} : $_, %alts };
